70eb84bddc214979a841328ca9a230ebd6b0d238
figo5530 <figooooo0221@gmail.com>
2021-01-13 14:20:25 -0500

    commit tree 1e5e275ad0a8fdfbb9e1e369714e26d8e4d8ad44

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(sentence)
+    arr = sentence.split
+    new_arr = []
+    arr.collect do |word|
+        new_arr << word.reverse
+    end
+    new_arr.join(" ")
+end
+
+puts reverse_each_word("Hello there, and how are you?")
